1ba54e2e2866e093f254664f25f43f288087f630
ibalram <balramsinghrathore3@gmail.com>
2020-10-29 01:33:12 +0530

    Added: Master branch

---
+++ src/main/java/com/workshop/leagueanalyser/Library.java
@@ -0,0 +1,7 @@
+package com.workshop.leagueanalyser;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/com/workshop/leagueanalyser/LeagueAnalyserTest.java
@@ -0,0 +1,14 @@
+package com.workshop.leagueanalyser;
+
+import org.junit.Test;
+
+import com.workshop.leagueanalyser.Library;
+
+import static org.junit.Assert.*;
+
+public class LeagueAnalyserTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
